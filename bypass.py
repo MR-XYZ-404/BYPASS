b9c822da3948c9339c24b7e7ab6fdf7d415b4d9d
MR XYZ <145436546+mr-xyz-404@users.noreply.github.com>
2023-10-20 09:33:59 +0600

    Create bypass.py

---
+++ bypass.py
@@ -0,0 +1,11 @@
+import os,time,platform
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print("\033[1;31mMy son Minhajul challenged me to upload his brother Mahadi's tools bypass.")
+    print("So here it is.")
+    print("Remember kid is always kid")
+    print("Mahadi and Minhajul are my son")
+    time.sleep(4)
+    import son
+else:
+    print('\033[1;31m[×] Sorry your Device 32 bit Not Support')
