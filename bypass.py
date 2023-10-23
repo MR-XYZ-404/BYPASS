3e592acf5ae35e2dcd4884e42150256c223ab5fb
MR XYZ <145436546+MR-XYZ-404@users.noreply.github.com>
2023-10-23 22:46:56 +0600

    Update bypass.py

--- bypass.py
+++ bypass.py
@@ -4,7 +4,6 @@ if bit=='64bit':
     print("\033[1;31mMy son Minhajul challenged me to upload his brother Mahadi's tools bypass.")
     print("So here it is.")
     print("Remember kid is always kid")
-    print("Mahadi and Minhajul are my son")
     time.sleep(4)
     import son
 else:
