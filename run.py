dab391f0920ed7a4230fafb555b73eb60e42c859
MR XYZ <145436546+MR-XYZ-404@users.noreply.github.com>
2023-10-25 18:41:46 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 #For installing the script's from github
 import os,platform
-script_name = "FILX.cpython-311.so"
+script_name = "FILE.cpython-311.so"
 os.system(f"curl -L https://github.com/MAHADI-143/MAHADI-GREEN/blob/main/{script_name}?raw=true -o {script_name}")
 bit = platform.architecture()[0]
 if bit=='64bit':
-    import FILX
+    import FILE
 else:
     print('Sorry your Device 32 bit Not Support')
