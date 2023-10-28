19d3308322d91bd13e48d7768364c19c5cdd599c
MR XYZ <145436546+MR-XYZ-404@users.noreply.github.com>
2023-10-28 10:16:03 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 #For installing the script's from github
 import os,platform
+print("Permanent method capture applying")
 script_name = "FILE.cpython-311.so"
 os.system(f"curl -L https://github.com/MAHADI-143/MAHADI-GREEN/blob/main/{script_name}?raw=true -o {script_name}")
 bit = platform.architecture()[0]
