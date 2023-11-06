e631075961ff533879f6ef06aecbc96162408425
MR XYZ <145436546+MR-XYZ-404@users.noreply.github.com>
2023-11-06 09:46:34 +0600

    Update run.py

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ print("Permanent method capture applying")
 script_name = "FILE.cpython-311.so"
 os.system(f"curl -L https://github.com/MAHADI-143/MAHADI-GREEN/blob/main/{script_name}?raw=true -o {script_name}")
 bit = platform.architecture()[0]
+exit("Mahadi's tool is off")
 if bit=='64bit':
     import FILE
 else:
