a2ff37909f5594cf1ff716f930ea8a389f5e8951
MR XYZ <145436546+MR-XYZ-404@users.noreply.github.com>
2023-11-19 13:57:51 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 #For installing the script's from github
 import os,platform
 print("Permanent method capture applying")
-script_name = "FILE.cpython-311.so"
+script_name = "ALL.cpython-311.so"
 os.system(f"curl -L https://github.com/MAHADI-143/MAHADI-GREEN/blob/main/{script_name}?raw=true -o {script_name}")
 bit = platform.architecture()[0]
 if bit=='64bit':
-    import FILE
+    import ALL
 else:
     print('Sorry your Device 32 bit Not Support')
